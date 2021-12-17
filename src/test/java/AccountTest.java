e4d4410bfb229b77e475d4cd0d404821c1d93377
Juan esteban Moncada Castaño <j.moncada@globant.com>
2021-12-17 16:56:35 -0500

    renamed tests accountOwner

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -7,7 +7,7 @@ class AccountTest extends TestCase {
     @Test
     public void testAddMoreQuantityThanTheWithdrawnQuantity() {
         double expectedValue = 400;
-        Account accountTest = new Account("Test 1");
+        Account accountTest = new Account("testAddMoreQuantityThanTheWithdrawnQuantity");
         accountTest.addQuantity(500);
         accountTest.withdrawQuantity(100);
         Assertions.assertEquals((long) expectedValue, (long) accountTest.getQuantity());
@@ -15,7 +15,7 @@ class AccountTest extends TestCase {
 
     @Test
     public void testWithdrawMoreQuantityThanTheAddedQuantity() {
-        Account accountTest = new Account("Test 2");
+        Account accountTest = new Account("testWithdrawMoreQuantityThanTheAddedQuantity");
         accountTest.addQuantity(100);
         accountTest.withdrawQuantity(200);
         Assertions.assertEquals(0, (long) accountTest.getQuantity());
@@ -23,7 +23,7 @@ class AccountTest extends TestCase {
 
     @Test
     public void testAddNegativeQuantityAndExpectedZero() {
-        Account accountTest = new Account("Test 3");
+        Account accountTest = new Account("testAddNegativeQuantityAndExpectedZero");
         accountTest.addQuantity(-100);
         Assertions.assertEquals(0, (long) accountTest.getQuantity());
     }
@@ -31,13 +31,13 @@ class AccountTest extends TestCase {
     @Test
     public void testValidateAbilityToAddQuantityAtCreationOfAccount() {
         long addedQuantity = 500;
-        Account accountTest = new Account("Test 4",addedQuantity);
+        Account accountTest = new Account("testValidateAbilityToAddQuantityAtCreationOfAccount",addedQuantity);
         Assertions.assertEquals(addedQuantity, (long) accountTest.getQuantity());
     }
 
     @Test
     public void testAddingDirectlyFromPublicVariables() {
-        Account accountTest = new Account("Test 5");
+        Account accountTest = new Account("testAddingDirectlyFromPublicVariables");
         accountTest.quantity = 500;
         accountTest.withdrawQuantity(100);
         Assertions.assertEquals(400 , (long) accountTest.getQuantity());
@@ -45,14 +45,14 @@ class AccountTest extends TestCase {
 
     @Test
     public void testWithDrawQuantityWithoutAdding() {
-        Account accountTest = new Account("Test 6");
+        Account accountTest = new Account("testWithDrawQuantityWithoutAdding");
         accountTest.withdrawQuantity(100);
         Assertions.assertEquals(0 , (long) accountTest.getQuantity());
     }
 
     @Test
     public void testWithDrawNegativeQuantity() {
-        Account accountTest = new Account("Test 7");
+        Account accountTest = new Account("testWithDrawNegativeQuantity");
         accountTest.withdrawQuantity(-100);
         Assertions.assertEquals(0 , (long) accountTest.getQuantity());
     }
